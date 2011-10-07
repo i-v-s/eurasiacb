6cbe67070561fd65472bada3117a442f6afc135f
pankratov.vs <pankratov.vs@gmail.com>
2011-10-07 16:48:38 +0000

    added ros QStab

---
+++ projects/ros/qstab/build/CMakeFiles/stabtalker.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "../msg_gen"
+  "../src/qstab/msg"
+  "../msg_gen"
+  "CMakeFiles/stabtalker.dir/src/stabtalker.o"
+  "../bin/stabtalker.pdb"
+  "../bin/stabtalker"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/stabtalker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
