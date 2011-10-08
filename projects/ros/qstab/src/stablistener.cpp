8b85375d0f35d60b7dbdda25d38d0adee191b3a1
bachinin <bachinin@9ae19bdd-40bc-9e85-e21a-924888043a86>
2011-10-08 09:57:14 +0000

    изменил тип Messege на Move

--- projects/ros/qstab/src/stablistener.cpp
+++ projects/ros/qstab/src/stablistener.cpp
@@ -1,12 +1,9 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include "qstab/Move.h"
 
-/**
- * This tutorial demonstrates simple receipt of messages over the ROS system.
- */
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
+void chatterCallback(const qstab::Move msg)
 {
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
+  ROS_INFO("I heard: [%ld]", (long int) msg.horizontal);
 }
 
 int main(int argc, char **argv)
