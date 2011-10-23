128e7125bc1719385dfe845917c13d961dbe936a
pankratov.vs <pankratov.vs@gmail.com>
2011-10-23 18:34:20 +0000

    :)

---
+++ projects/ros/test/src/publoc.cpp
@@ -0,0 +1,44 @@
+#include "ros/ros.h"
+#include "std_msgs/Int32.h"
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <sstream>
+
+int main(int argc, char **argv)
+{
+
+  ros::init(argc, argv, "publoc");
+
+  ros::NodeHandle n;
+
+  ros::Publisher chatter_pub = n.advertise<std_msgs::Int32>("chatter", 1000);
+
+  ros::Rate loop_rate(10);
+
+  cv::Mat img = cv::imread("delacroix.jpg");
+
+  int count = 0;
+  while (ros::ok())
+  {
+    std_msgs::Int32 msg;
+
+    msg.data = img.rows;
+
+    ROS_INFO("%d", msg.data);
+
+    chatter_pub.publish(msg);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+    ++count;
+  }
+
+
+  return 0;
+}
+
+
