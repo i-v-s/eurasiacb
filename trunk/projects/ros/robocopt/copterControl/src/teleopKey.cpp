0f5146e81f0bb0bb29239521b76b64de3ca8d056
pankratov.vs@gmail.com <pankratov.vs@gmail.com@9ae19bdd-40bc-9e85-e21a-924888043a86>
2012-01-03 22:46:01 +0000

    changed teleopKey file

--- trunk/projects/ros/robocopt/copterControl/src/teleopKey.cpp
+++ trunk/projects/ros/robocopt/copterControl/src/teleopKey.cpp
@@ -4,12 +4,29 @@
 #include <termios.h>
 #include <stdio.h>
 
+// Key codes
 #define KEYCODE_R 0x43
 #define KEYCODE_L 0x44
 #define KEYCODE_U 0x41
-#define KEYCODE_D 0x42
-#define KEYCODE_S 0x20
+#define KEYCODE_DW 0x42
+#define KEYCODE_W 0x77
+#define KEYCODE_S 0x73
+#define KEYCODE_D 0x64
+#define KEYCODE_SP 0x20
 #define KEYCODE_Q 0x71
+
+// Params, DEF- defoult
+#define DEF_X 0
+#define MAX_X 10
+#define MIN_X -10
+#define DEF_Y 0
+#define MAX_Y 100
+#define MIN_Y -100
+#define DEF_Z 0
+#define MAX_Z 10
+#define MIN_Z -10
+
+#define DATAGET_SPEED 4
 
 class TeleopKey
 {
@@ -21,12 +38,19 @@ class TeleopKey
 
         ros::NodeHandle nh_;
         ros::Publisher ctrl_pub_;
+        copterControl::CControl ctrl_msg_;
 
+        int z_factor;
 };
 
 TeleopKey::TeleopKey()
 {
     ctrl_pub_ = nh_.advertise<copterControl::CControl>("copterControl/teleopKey", 1);
+
+    ctrl_msg_.x = DEF_X;
+    ctrl_msg_.y = DEF_Y;
+    ctrl_msg_.z = DEF_Z;
+    ctrl_msg_.fix = false;
 }
 
 int kfd = 0;
@@ -82,50 +106,63 @@ void TeleopKey::keyLoop()
         }
 
         ROS_DEBUG("value: 0x%02X\n", c);
-        copterControl::CControl ctrl;
-        ctrl.x = 0;
-        ctrl.y = 0;
-        ctrl.z = 0;
-        ctrl.fix = false;
+
+        ctrl_msg_.x = DEF_X;
+        ctrl_msg_.z = DEF_Z;
 
         switch(c)
         {
             case KEYCODE_L:
                 ROS_DEBUG("LEFT");
-                ctrl.x = -1;
+                ctrl_msg_.x = MIN_X;
                 dirty = true;
             break;
             case KEYCODE_R:
                 ROS_DEBUG("RIGHT");
-                ctrl.x = 1;
+                ctrl_msg_.x = MAX_X;
                 dirty = true;
             break;
             case KEYCODE_U:
-                ROS_DEBUG("UP");
-                ctrl.y = 1;
+                ROS_DEBUG("FORWARD");
+                ctrl_msg_.z = MAX_Z;
                 dirty = true;
             break;
-            case KEYCODE_D:
-                ROS_DEBUG("DOWN");
-                ctrl.y = -1;
+            case KEYCODE_DW:
+                ROS_DEBUG("BACK");
+                ctrl_msg_.z = MIN_Z;
+                dirty = true;
+            break;
+            case KEYCODE_W:
+                ROS_DEBUG("UP");
+                ctrl_msg_.y < MAX_Y ? ctrl_msg_.y++ : ctrl_msg_.y= MAX_Y;
                 dirty = true;
             break;
             case KEYCODE_S:
-                ROS_DEBUG("SPACE");
-                ctrl.fix = true;
+                ROS_DEBUG("DOWN");
+                ctrl_msg_.y > MIN_Y ? ctrl_msg_.y-- : ctrl_msg_.y = MIN_Y;
                 dirty = true;
             break;
+            case KEYCODE_SP:
+                ROS_DEBUG("STOP");
+                ctrl_msg_.x = DEF_X;
+                ctrl_msg_.z = DEF_Z;
+                dirty = true;
+            break;
+            case KEYCODE_D:
+                ROS_DEBUG("FIX");
+                ctrl_msg_.fix = true;
+                dirty = true;
+            break;
+
         }
 
-        if(dirty ==true)
-        {
-            ctrl_pub_.publish(ctrl);
+        if(dirty) {
+            ROS_INFO("x=%d, y=%d, z=%d, fix=%s", ctrl_msg_.x, ctrl_msg_.y, ctrl_msg_.z, ctrl_msg_.fix?"true":"false");
+            ctrl_pub_.publish(ctrl_msg_);
+            ctrl_msg_.fix = false;
             dirty=false;
         }
     }
-
-    ROS_INFO("This is the end");
-
 
     return;
 }
