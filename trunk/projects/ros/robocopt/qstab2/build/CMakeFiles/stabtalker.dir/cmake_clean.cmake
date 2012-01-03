b3d73a8f1180db3d0f9d059cc224a2535c14e579
pankratov.vs@gmail.com <pankratov.vs@gmail.com@9ae19bdd-40bc-9e85-e21a-924888043a86>
2012-01-03 19:51:14 +0000

    Stack created

---
+++ trunk/projects/ros/robocopt/qstab2/build/CMakeFiles/stabtalker.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/stabtalker.dir/src/stabtalker.o"
+  "CMakeFiles/stabtalker.dir/home/vasiliy/svn/eurasiacb/projects/cpp/qstab/1.0.1/QStab/qstab.o"
+  "CMakeFiles/stabtalker.dir/home/vasiliy/svn/eurasiacb/projects/cpp/qstab/1.0.1/QStab/videoprocessor.o"
+  "../bin/stabtalker.pdb"
+  "../bin/stabtalker"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/stabtalker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
