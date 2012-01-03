90209d976f168f69520e66cdf29147329cdb5be4
pankratov.vs <pankratov.vs@gmail.com>
2012-01-03 11:43:15 +0000

    qstab2 is good folder

---
+++ projects/ros/qstab2/build/CMakeFiles/stabtalker.dir/DependInfo.cmake
@@ -0,0 +1,15 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/vasiliy/svn/eurasiacb/projects/cpp/qstab/1.0.1/QStab/qstab.cpp" "/home/vasiliy/svn/eurasiacb/projects/ros/qstab2/build/CMakeFiles/stabtalker.dir/home/vasiliy/svn/eurasiacb/projects/cpp/qstab/1.0.1/QStab/qstab.o"
+  "/home/vasiliy/svn/eurasiacb/projects/cpp/qstab/1.0.1/QStab/videoprocessor.cpp" "/home/vasiliy/svn/eurasiacb/projects/ros/qstab2/build/CMakeFiles/stabtalker.dir/home/vasiliy/svn/eurasiacb/projects/cpp/qstab/1.0.1/QStab/videoprocessor.o"
+  "/home/vasiliy/svn/eurasiacb/projects/ros/qstab2/src/stabtalker.cpp" "/home/vasiliy/svn/eurasiacb/projects/ros/qstab2/build/CMakeFiles/stabtalker.dir/src/stabtalker.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
