c34a9e25edafdb8ff5dd07feac0e7f106030fc94
pankratov.vs <pankratov.vs@gmail.com>
2012-01-03 19:51:14 +0000

    Stack created

---
+++ projects/ros/qstab2/build/CMakeFiles/teleopkey.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/teleopkey.dir/src/teleopKey.o"
+  "../bin/teleopkey.pdb"
+  "../bin/teleopkey"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/teleopkey.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
