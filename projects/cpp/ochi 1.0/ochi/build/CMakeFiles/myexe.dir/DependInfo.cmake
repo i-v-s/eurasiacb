7878f47cb97b38a7ffb986ef0b58b9f9ac6fc4d2
bachinin <bachinin@gmail.com>
2012-02-25 13:05:25 +0000

    aaa

---
+++ projects/cpp/ochi 1.0/ochi/build/CMakeFiles/myexe.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/bachinin/projects/eurasiacb/projects/cpp/ochi 1.0/ochi/filter.cpp" "/home/bachinin/projects/eurasiacb/projects/cpp/ochi 1.0/ochi/build/CMakeFiles/myexe.dir/filter.cpp.o"
+  "/home/bachinin/projects/eurasiacb/projects/cpp/ochi 1.0/ochi/main.cpp" "/home/bachinin/projects/eurasiacb/projects/cpp/ochi 1.0/ochi/build/CMakeFiles/myexe.dir/main.cpp.o"
+  "/home/bachinin/projects/eurasiacb/projects/cpp/ochi 1.0/ochi/matcher.cpp" "/home/bachinin/projects/eurasiacb/projects/cpp/ochi 1.0/ochi/build/CMakeFiles/myexe.dir/matcher.cpp.o"
+  "/home/bachinin/projects/eurasiacb/projects/cpp/ochi 1.0/ochi/matrix.cpp" "/home/bachinin/projects/eurasiacb/projects/cpp/ochi 1.0/ochi/build/CMakeFiles/myexe.dir/matrix.cpp.o"
+  "/home/bachinin/projects/eurasiacb/projects/cpp/ochi 1.0/ochi/reconstruction.cpp" "/home/bachinin/projects/eurasiacb/projects/cpp/ochi 1.0/ochi/build/CMakeFiles/myexe.dir/reconstruction.cpp.o"
+  "/home/bachinin/projects/eurasiacb/projects/cpp/ochi 1.0/ochi/triangle.cpp" "/home/bachinin/projects/eurasiacb/projects/cpp/ochi 1.0/ochi/build/CMakeFiles/myexe.dir/triangle.cpp.o"
+  "/home/bachinin/projects/eurasiacb/projects/cpp/ochi 1.0/ochi/viso.cpp" "/home/bachinin/projects/eurasiacb/projects/cpp/ochi 1.0/ochi/build/CMakeFiles/myexe.dir/viso.cpp.o"
+  "/home/bachinin/projects/eurasiacb/projects/cpp/ochi 1.0/ochi/viso_mono.cpp" "/home/bachinin/projects/eurasiacb/projects/cpp/ochi 1.0/ochi/build/CMakeFiles/myexe.dir/viso_mono.cpp.o"
+  "/home/bachinin/projects/eurasiacb/projects/cpp/ochi 1.0/ochi/viso_stereo.cpp" "/home/bachinin/projects/eurasiacb/projects/cpp/ochi 1.0/ochi/build/CMakeFiles/myexe.dir/viso_stereo.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ projects/cpp/ochi 1.0/ochi/build/CMakeFiles/viso2.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/viso2.dir/viso_mono.cpp.o"
+  "CMakeFiles/viso2.dir/matrix.cpp.o"
+  "CMakeFiles/viso2.dir/triangle.cpp.o"
+  "CMakeFiles/viso2.dir/viso_stereo.cpp.o"
+  "CMakeFiles/viso2.dir/main.cpp.o"
+  "CMakeFiles/viso2.dir/viso.cpp.o"
+  "CMakeFiles/viso2.dir/matcher.cpp.o"
+  "CMakeFiles/viso2.dir/filter.cpp.o"
+  "CMakeFiles/viso2.dir/reconstruction.cpp.o"
+  "viso2.pdb"
+  "viso2"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/viso2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
