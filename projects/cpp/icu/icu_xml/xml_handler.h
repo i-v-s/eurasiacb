f0a331a11ba93af91b1dff6e82edbe0f2b3af290
bachinin <bachinin@gmail.com>
2012-04-08 17:43:36 +0000

    changed xml_handler

--- projects/cpp/icu/icu_xml/xml_handler.h
+++ projects/cpp/icu/icu_xml/xml_handler.h
@@ -16,8 +16,12 @@ public:
     XML_handler();
     XML_handler(string filename);
     void rootName(string rootname);
-    void addParam(string paramName, string paramValue);
-    void addParam(string paramName, const Mat& paramValue);
+    template <class T>
+    void addParam(string paramName,T paramValue){
+        Element* elem = root->add_child(paramName);
+        elem->add_child_text(str_convert(paramValue));
+    }
+    //void addParam(string paramName, const Mat& paramValue);
     void save(string filename);
 private:
     Document doc;
